5a48f8c0b7a558774bfb30754b14fa02acc46952
TTaket <575500603@qq.com>
2025-03-08 17:40:26 +0800

    docs: update alias

---
+++ scripts/alias.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# docker
+alias begin_docker="docker-compose -f ./docker-compose.yml up -d"
+alias end_docker="docker-compose -f ./docker-compose.yml down"
+alias restart_docker="docker-compose -f ./docker-compose.yml restart"
+
+# server and client
+alias run_experiment_server="go run ./apps/Micro/experiment/cmd/server_main/server.go"
+alias run_experiment_client="go run ./apps/Micro/experiment/cmd/client_main/client.go"
+alias run_userb_server="go run ./apps/Micro/userb/cmd/server_main/server.go"
+alias run_userb_client="go run ./apps/Micro/userb/cmd/client_main/client.go"
+
+
+# etcd
+ETCD_HOST=127.0.0.1
+ETCD_ENDPOINTS=${ETCD_HOST}:12379
+alias docker_etcdctl_all="etcdctl --endpoints=${ETCD_ENDPOINTS} get --prefix '' "
+alias etcdctl_all="etcdctl  get --prefix '' "
+
+# mysql
+alias docker_mysql="mysql -h 127.0.0.1 -P 13306 -u root -p'abtest2025'"
+
+# proto
+alias protoc_gen="sh ./scripts/genproto.sh"
